913a757bdcca47fa07f0d93868e67daf25752d62
unknown <alinlipotchi@yahoo.com>
2022-10-19 21:49:23 +0300

    adding mobile version

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const rightArrow = document.querySelector("#right-arrow");
 // Slider Function
 
 const slider = async () => {
-
     let i = 0;
     let n = 3;
 
@@ -45,20 +44,47 @@ const slider = async () => {
     rightArrow.addEventListener("click", goRight);
 
     // Planet Changer
-
-    console.log(img.children[1].children[3].children[0].children[1]);
-    console.log(planets[i+1].name);
     const change = (e) => {
-        img.children[0].style.background = `url(${planets[e + 1].image})`;
-        img.children[0].children[0].innerHTML = `<h3>${planets[i + 1].name.toUpperCase()}</h3>
+        img.children[0].style.background = `url(${
+            planets[e + 1].image
+        }) no-repeat center center/cover`;
+        img.children[0].children[0].innerHTML = `<h3>${planets[
+            i + 1
+        ].name.toUpperCase()}</h3>
         <p>${planets[i + 1].location}</p>
         <p>Distance: ${planets[i + 1].distance}</p>
         <p>AUPopulation: ${planets[i + 1].aupopulation}</p>`;
-        img.children[1].children[0].innerHTML = `${planets[i + 1].name.charAt(0).toUpperCase()}<span class="fw-2">${planets[i + 1].name.slice(1)}</span>`;
+        img.children[1].children[0].innerHTML = `${planets[i + 1].name
+            .charAt(0)
+            .toUpperCase()}<span class="fw-2">${planets[i + 1].name.slice(
+            1
+        )}</span>`;
         img.children[1].children[1].innerHTML = planets[i + 1].description;
-        img.children[1].children[3].children[0].children[0].innerHTML = `${planets[i + 1].price}&euro;`;
-        img.children[1].children[3].children[0].children[1].innerHTML = `${planets[i + 1].ticket_type}`;
+        img.children[1].children[3].children[0].children[0].innerHTML = `${
+            planets[i + 1].price
+        }&euro;`;
+        img.children[1].children[3].children[0].children[1].innerHTML = `${
+            planets[i + 1].ticket_type
+        }`;
     };
-    console.log("moon".charAt(0));
 };
 slider();
+
+// Hamburger
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#hidden-menu");
+let toggle = true;
+
+const toggleMenu = (e) => {
+    e.preventDefault();
+    if (toggle) {
+        menu.style.transform = "translateY(0)";
+        toggle = false;
+    } else {
+        menu.style.transform = "translateY(-125%)";
+        toggle = true;
+    }
+};
+
+hamburger.addEventListener("click", toggleMenu);
